70d728123f22083ab3bb03c0d7135986ab638953
Edoardo Piragine <edoardo.piragine@gmail.com>
2021-03-18 15:32:29 +0100

    init

---
+++ database/migrations/2021_03_18_131230_create_beers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBeersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('beers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 30);
+            $table->string('alcohol_content', 4);
+            $table->string('description', 1000);
+            $table->float('price', 4, 2);
+            $table->string('img_url', 2048);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('beers');
+    }
+}
---
+++ resources/views/beers/index.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BEERS</title>
+</head>
+<body>
+
+    <table class="table">
+
+        <thead class="thead-dark">
+
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Alcohol content</th>
+            <th scope="col">Description</th>
+            <th scope="col">Price</th>
+            <th scope="col">Img</th>
+          </tr>
+
+        </thead>
+
+        <tbody>
+
+            @foreach ($beers as $beer)
+
+                <tr>
+                    <th scope="row">{{$beer->id}}</th>
+                    <td>{{$beer->name}}</td>
+                    <td>{{$beer->alcohol_content}}</td>
+                    <td>{{$beer->description}}</td>
+                    <td>{{$beer->price}}</td>
+                    <td><img src="{{$beer->img_url}}" width="100"></td>
+                </tr>
+
+            @endforeach
+
+        </tbody>
+
+      </table>
+
+</body>
+</html>
