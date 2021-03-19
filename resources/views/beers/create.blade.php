78f8f7b333efb0c391daeda5d46f8acf4d485d6f
Edoardo Piragine <edoardo.piragine@gmail.com>
2021-03-19 15:51:27 +0100

    refactoring

--- resources/views/beers/create.blade.php
+++ resources/views/beers/create.blade.php
@@ -1,83 +1,139 @@
 @extends('layout.mainLayout')
 @section('main-content')
 
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-
-    <form action="{{route('beers.store')}}" method="post" class="needs-validation" novalidate>
+    <form action="{{route('beers.store')}}"
+            method="post"
+            class="needs-validation"
+            novalidate
+    >
 
         @csrf
         @method('POST')
 
         <div class="form-group">
+
             <label for="name">Name</label>
-            <input type="text" class="form-control" name="name" placeholder="Name" required>
+
+            <input type="text"
+                    class="form-control"
+                    name="name"
+                    placeholder="Name"
+                    required
+            >
+
             <div class="invalid-feedback">
                 Please provide a valid name.
             </div>
+
             <div class="valid-feedback">
                 Name's good!
             </div>
+
         </div>
 
         <div class="form-group">
+
             <label for="alcohol_content">Alcohol content</label>
-            <input type="text" name="alcohol_content" class="form-control" placeholder="Alcohol content" required><div class="invalid-feedback">
+
+            <input type="text"
+                    name="alcohol_content"
+                    class="form-control"
+                    placeholder="Alcohol content"
+                    required
+            >
+
+            <div class="invalid-feedback">
                 Please provide a valid alcohol content.
             </div>
+
             <div class="valid-feedback">
                 Alcohol content's good!
             </div>
+
         </div>
 
         <div class="form-group">
+
             <label for="description">Description</label>
-            <input type="text" name="description" class="form-control" placeholder="Description" required><div class="invalid-feedback">
+
+            <input type="text"
+                    name="description"
+                    class="form-control"
+                    placeholder="Description"
+                    required
+            >
+
+            <div class="invalid-feedback">
                 Please provide a valid description.
             </div>
+
             <div class="valid-feedback">
                 Description's good!
             </div>
+
         </div>
 
         <div class="form-group">
+
             <label for="price">Price</label>
-            <input type="text" name="price" class="form-control" placeholder="Price" required>
+
+            <input type="text"
+                    name="price"
+                    class="form-control"
+                    placeholder="Price"
+                    required
+            >
+
             <div class="invalid-feedback">
                 Please provide a valid price.
             </div>
+
             <div class="valid-feedback">
                 Price's good!
             </div>
+
         </div>
 
         <div class="form-group">
+
             <label for="img_url">Img URL</label>
-            <input type="text" name="img_url" class="form-control" placeholder="Img URL" required>
+
+            <input type="text"
+                    name="img_url"
+                    class="form-control"
+                    placeholder="Img URL"
+                    required
+            >
+
             <div class="invalid-feedback">
                 Please provide a valid Img URL.
             </div>
+
             <div class="valid-feedback">
                 Img URL's good!
             </div>
+
         </div>
 
         <div class="form-group">
+
             <label for="origin">Origin</label>
-            <input type="text" name="origin" class="form-control" placeholder="Origin" required>
+
+            <input type="text"
+                    name="origin"
+                    class="form-control"
+                    placeholder="Origin"
+                    required
+            >
+
             <div class="invalid-feedback">
                 Please provide a valid origin.
             </div>
+
             <div class="valid-feedback">
                 Origin's good!
             </div>
+
         </div>
 
         <input type="submit" value="Submit">
