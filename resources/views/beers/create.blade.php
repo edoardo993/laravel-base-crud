c7365158ae0370221966b3857208bfa0f9c2baad
Edoardo Piragine <edoardo.piragine@gmail.com>
2021-03-19 15:42:04 +0100

    boostrap validation control added

--- resources/views/beers/create.blade.php
+++ resources/views/beers/create.blade.php
@@ -11,39 +11,73 @@
         </div>
     @endif
 
-    <form action="{{route('beers.store')}}" method="post">
+    <form action="{{route('beers.store')}}" method="post" class="needs-validation" novalidate>
 
         @csrf
         @method('POST')
 
         <div class="form-group">
             <label for="name">Name</label>
-            <input type="text" class="form-control" name="name" placeholder="Name">
+            <input type="text" class="form-control" name="name" placeholder="Name" required>
+            <div class="invalid-feedback">
+                Please provide a valid name.
+            </div>
+            <div class="valid-feedback">
+                Name's good!
+            </div>
         </div>
 
         <div class="form-group">
             <label for="alcohol_content">Alcohol content</label>
-            <input type="text" name="alcohol_content" class="form-control" placeholder="Alcohol content">
+            <input type="text" name="alcohol_content" class="form-control" placeholder="Alcohol content" required><div class="invalid-feedback">
+                Please provide a valid alcohol content.
+            </div>
+            <div class="valid-feedback">
+                Alcohol content's good!
+            </div>
         </div>
 
         <div class="form-group">
             <label for="description">Description</label>
-            <input type="text" name="description" class="form-control" placeholder="Description">
+            <input type="text" name="description" class="form-control" placeholder="Description" required><div class="invalid-feedback">
+                Please provide a valid description.
+            </div>
+            <div class="valid-feedback">
+                Description's good!
+            </div>
         </div>
 
         <div class="form-group">
             <label for="price">Price</label>
-            <input type="text" name="price" class="form-control" placeholder="Price">
+            <input type="text" name="price" class="form-control" placeholder="Price" required>
+            <div class="invalid-feedback">
+                Please provide a valid price.
+            </div>
+            <div class="valid-feedback">
+                Price's good!
+            </div>
         </div>
 
         <div class="form-group">
             <label for="img_url">Img URL</label>
-            <input type="text" name="img_url" class="form-control" placeholder="Img URL">
+            <input type="text" name="img_url" class="form-control" placeholder="Img URL" required>
+            <div class="invalid-feedback">
+                Please provide a valid Img URL.
+            </div>
+            <div class="valid-feedback">
+                Img URL's good!
+            </div>
         </div>
 
         <div class="form-group">
             <label for="origin">Origin</label>
-            <input type="text" name="origin" class="form-control" placeholder="Origin">
+            <input type="text" name="origin" class="form-control" placeholder="Origin" required>
+            <div class="invalid-feedback">
+                Please provide a valid origin.
+            </div>
+            <div class="valid-feedback">
+                Origin's good!
+            </div>
         </div>
 
         <input type="submit" value="Submit">
--- resources/views/layout/mainLayout.blade.php
+++ resources/views/layout/mainLayout.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/app.js') }}"></script>
     <title>BEERS</title>
 </head>
 <body>
